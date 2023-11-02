7ee687c923a785855a909607d9099fae6d193ec2
divaaga <93255055+divaaga@users.noreply.github.com>
2023-11-02 22:23:06 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+song1 = "";
+function preload() {
+    song1 = loadSound("music.mp3");
+    song2 = loadSound("music2.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.size(600, 500);
+    video.hide();
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
+
+function modelLoaded() {
+   
+}
+
+function gotPoses() {
+}
