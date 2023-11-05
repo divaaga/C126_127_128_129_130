9cf22d31bbf5710faf72db2652eacc4e4f6eb7d7
divaaga <93255055+divaaga@users.noreply.github.com>
2023-11-05 12:13:10 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 song1 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
 function preload() {
     song1 = loadSound("music.mp3");
     song2 = loadSound("music2.mp3");
@@ -11,6 +16,9 @@ function setup() {
     video = createCapture(VIDEO);
     video.size(600, 500);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
 }
 
 function draw() {
@@ -18,8 +26,19 @@ function draw() {
 }
 
 function modelLoaded() {
-   
+    console.log("posenet is now initialized...");
 }
 
 function gotPoses() {
+    if (results.length > 0) {
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x ;
+        leftWristY = results[0].pose.leftWrist.y ;
+        console.log("left wrist X = " + leftWristX + " left wrist Y = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x ;
+        rightWristY = results[0].pose.rightWrist.y ;
+        console.log("right wrist X = " + rightWristX + " right wrist Y = " + rightWristY);
+    }
 }
